20d6ac31c906f3ab96f16d80a8caeedcd7aee9c5
rkq00 <60001560+rk-q09@users.noreply.github.com>
2022-06-20 15:57:26 +0900

    feat: overall design changes

--- src/features/misc/routes/Landing.tsx
+++ src/features/misc/routes/Landing.tsx
@@ -1,5 +1,5 @@
 import { useNavigate } from 'react-router';
-import { Flex, Box, Text, VStack, Button } from '@chakra-ui/react';
+import { Flex, Heading, VStack, Button } from '@chakra-ui/react';
 
 import { useAuth } from '@/lib/auth';
 
@@ -16,15 +16,13 @@ export const Landing = () => {
   };
 
   return (
-    <Flex align="center" justify="center" height="100vh">
-      <Box bg="white" h="lg" w="lg" borderRadius="lg">
-        <VStack m={40}>
-          <Text color="black">Welcome to QUIZPRO</Text>
-          <Button onClick={handleStart} colorScheme="teal">
-            get started
-          </Button>
-        </VStack>
-      </Box>
+    <Flex align="center" justify="center" height="100vh" color="white">
+      <VStack>
+        <Heading size="4xl" mb={3}>Welcome to QUIZPRO</Heading>
+        <Button onClick={handleStart} colorScheme="teal">
+          get started
+        </Button>
+      </VStack>
     </Flex>
   );
 };
