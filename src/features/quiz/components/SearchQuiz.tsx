d308ae3ce0a5d08c3d7cc3412f878c75d7877ee5
rkq00 <60001560+rk-q09@users.noreply.github.com>
2022-07-08 10:14:01 +0900

    feat: change search bar to use RHF

--- src/features/quiz/components/SearchQuiz.tsx
+++ src/features/quiz/components/SearchQuiz.tsx
@@ -1,42 +1,53 @@
-import { useState } from 'react';
-import { FormControl, Input, Icon, Flex } from '@chakra-ui/react';
+import { z } from 'zod';
+import { Input, Icon, Flex, Button } from '@chakra-ui/react';
 import { useNavigate } from 'react-router-dom';
 import { SearchIcon } from '@heroicons/react/solid';
 
-export const SearchQuiz = () => {
-  const [input, setInput] = useState<string>("");
+import { Form } from '@/components/Form';
 
+const schema = z.object({
+  input: z.string().min(1)
+});
+
+type SearchQuizValues = {
+  input: string;
+};
+
+export const SearchQuiz = () => {
   const navigate = useNavigate();
 
-  const handleInputChange = (e: any) => setInput(e.target.value);
-
-  const onSubmit = () => {
-    navigate(`/app/quiz/search/${input}`);
-    setInput("");
-  }
-
-
   return (
-    <FormControl>
-      <Flex align="center">
-        <Input 
-          type="text" 
-          borderRadius="3xl"
-          bg="tertiary.100"
-          color="black"
-          autoComplete="off"
-          placeholder="Search quiz"
-          value={input} 
-          onChange={handleInputChange} 
-        />
-        <Icon 
-          as={SearchIcon} 
-          w={6} 
-          h={6} 
-          _hover={{ cursor: 'pointer' }}
-          onClick={onSubmit}
-        />
-      </Flex>
-    </FormControl>
+    <Form<SearchQuizValues>
+      onSubmit={(values) => {
+        navigate(`/app/quiz/search/${values.input}`);
+      }}
+      schema={schema}
+    >
+      {({ register }) => (
+        <Flex align="center">
+          <Input 
+            {...register("input")}
+            type="text" 
+            borderRadius="3xl"
+            bg="tertiary.100"
+            color="black"
+            autoComplete="off"
+            placeholder="Search quiz"
+          />
+          <Button 
+            type="submit"
+            bg="none"
+            _hover={{ bg: 'none' }}
+          >
+            <Icon 
+              as={SearchIcon} 
+              w={6} 
+              h={6} 
+              _hover={{ cursor: 'pointer', color: 'secondary.400' }}
+            />
+          </Button>
+        </Flex>
+      )}
+    </Form>
   );
 }
